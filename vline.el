fa61b2c6326a2c4d45648a5c127fb54a9e54fa3b
Taiki Sugawara <buzz.taiki@gmail.com>
2021-08-06 00:00:12 +0900

    simplify commentary

--- vline.el
+++ vline.el
@@ -1,4 +1,4 @@
-;;; vline.el --- Show vertical line (column highlighting) mode -*- lexical-binding: t -*-
+;;; vline.el --- column highlighting (vertical line displaying) mode -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2002, 2008-2021 by Taiki SUGAWARA <buzz.taiki@gmail.com>
 
@@ -26,23 +26,8 @@
 
 ;;; Commentary:
 
-;; put followings your .emacs
-;;   (require 'vline)
-;;
-;; if you display a vertical line, type M-x vline-mode.  `vline-mode' doesn't
-;; effect other buffers, because it is a buffer local minor mode.  if you hide
-;; a vertical line, type M-x vline-mode again.
-;;
-;; if you display a vertical line in all buffers, type M-x vline-global-mode.
-;;
-;; `vline-style' provides a display style of vertical line.  see
-;; `vline-style' docstring.
-;;
-;; if you don't want to visual line highlighting (ex.  for performance
-;; issue), please to set `vline-visual' to nil.
-;;
-;; if you don't want to use timer (ex.  you want to highlight column
-;; during moving cursors), please to set `vline-use-timer' to nil.
+;; `vline-mode' is a minor mode for highlighting column at cursor
+;; position.  To enable it in a buffer, type M-x vline-mode.
 
 ;;; Change Log:
 
